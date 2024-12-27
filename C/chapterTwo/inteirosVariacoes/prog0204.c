e662faae1e2edda1009dc1fc62c38ca6aa3b53e6
Allex-Lima <alexlmangabeira@gmail.com>
2024-12-27 16:23:11 -0300

    Feat: tamanho em bytes de um inteiro

---
+++ C/chapterTwo/inteirosVariacoes/prog0204.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// Programa que indica qual o número de bytes que ocupa um inteiro.
+int main() {
+    printf("O tamanho em bytes de um inteiro = %d bytes\n", sizeof(int));
+    
+    return 0;
+}
