7296ebe6dc6d1427b92d7b07c66781c4abdda0d6
Allex-Lima <alexlmangabeira@gmail.com>
2025-01-02 17:36:46 -0300

    signed_e_unsigned

---
+++ C/chapterTwo/signed_e_unsigned/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    unsigned int idade = 23;
+    signed int idade0 = 45;
+
+    printf("Usigned: %u\n", idade);
+    printf("Sign: %u", idade0);
+
+    return 0;
+}
